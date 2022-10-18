e691d46ab141dc0160016db08969cbc5c92782b9
AndrewKim330 <rendezvous330@gmail.com>
2022-10-19 00:00:26 +0900

    Initial commit

---
+++ src/main/java/com/example/interceptor/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.interceptor.handler;
+
+import com.example.interceptor.annotation.Auth;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.security.auth.message.AuthException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity authException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
